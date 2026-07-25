ddb12387451349c090d579a2665d7d9af7944115
VoltCam Developer <dev@voltcam.io>
2026-07-25 21:19:27 +0100

    feat(social): add SocialScreen wrapper layout

---
+++ voltcam/lib/features/social/social_screen.dart
@@ -0,0 +1,12 @@
+export 'social_feed_screen.dart';
+import 'package:flutter/material.dart';
+import 'social_feed_screen.dart';
+
+class SocialScreen extends StatelessWidget {
+  const SocialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SocialFeedScreen();
+  }
+}

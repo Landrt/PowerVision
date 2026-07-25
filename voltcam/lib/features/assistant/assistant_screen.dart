c0b37277bbd69648c0b0ac6a312769d6e3628b79
VoltCam Developer <dev@voltcam.io>
2026-07-25 21:19:27 +0100

    feat(assistant): add AssistantScreen layout container

---
+++ voltcam/lib/features/assistant/assistant_screen.dart
@@ -0,0 +1,12 @@
+export 'assistant_chat_screen.dart';
+import 'package:flutter/material.dart';
+import 'assistant_chat_screen.dart';
+
+class AssistantScreen extends StatelessWidget {
+  const AssistantScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AssistantChatScreen();
+  }
+}
